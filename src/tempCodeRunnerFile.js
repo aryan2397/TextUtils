5ef9ea5c607044714a77ec8c20eae982d74e47c9
aryan2397 <135629240+aryan2397@users.noreply.github.com>
2023-08-07 00:04:47 +0530

    Added Files

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,2 +1,9 @@
-
-    </>
+const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
